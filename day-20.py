b90ab78448796ef66662cb8fa6fb6b618c605fd9
AskSisyphus <75997124+AskSisyphus@users.noreply.github.com>
2021-02-03 19:02:02 -0500

    Update day-20.py

--- day-20.py
+++ day-20.py
@@ -1,5 +1,6 @@
 from turtle import Screen, Turtle
 import time
+from snake import Snake
 
 screen = Screen()
 
@@ -8,21 +9,10 @@ screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-segments = []
-
-for position in starting_positions:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
-
 game_is_on = True
 while game_is_on:
     screen.update()
-    for seg in segments:
-        seg.forward(20)
-
+    time.sleep(0.1)
+    snake.move()
 
 screen.exitonclick()
