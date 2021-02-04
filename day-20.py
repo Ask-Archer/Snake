540c3f299442d03e4facefd566758306facb9b51
AskSisyphus <75997124+AskSisyphus@users.noreply.github.com>
2021-02-03 19:36:24 -0500

    Update day-20.py

--- day-20.py
+++ day-20.py
@@ -13,6 +13,10 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    snake.move()
+    screen.listen()
+    screen.onkey(snake.up, "Up")
+    screen.onkey(snake.down, "Down")
+    screen.onkey(snake.left, "Left")
+    screen.onkey(snake.right, "Right")
 
-screen.exitonclick()
+    snake.move()
