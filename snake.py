07f608ad2fe71d0836be86c090e911c3f6ce3b81
AskSisyphus <75997124+AskSisyphus@users.noreply.github.com>
2021-02-05 14:07:24 -0500

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,4 +1,4 @@
-from turtle import Turtle
+ turtle import Turtle, Screen
 
 
 
@@ -7,7 +7,6 @@ UP = 90
 DOWN = 270
 LEFT = 180
 RIGHT = 0
-
 class Snake:
 
     def __init__(self):
@@ -29,19 +28,29 @@ class Snake:
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
         self.head.forward(20)
-        
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment((self.segments[-1]).position())
+
     def up(self):
         if self.head.heading() != DOWN:
             self.head.setheading(UP)
-            
+
     def down(self):
         if self.head.heading() != UP:
             self.head.setheading(DOWN)
-            
+
     def left(self):
         if self.head.heading() != RIGHT:
             self.head.setheading(LEFT)
-            
+
     def right(self):
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
